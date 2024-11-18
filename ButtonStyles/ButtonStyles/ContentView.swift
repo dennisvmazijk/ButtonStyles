403c637b713522e35c92bbd5e3caab3d6da05b7c
Dennis van Mazijk <dennis.v.mazijk@ubiregi.com>
2024-11-18 16:30:03 +0900

    buttonGroupViewとtoggleViewを追加

--- ButtonStyles/ButtonStyles/ContentView.swift
+++ ButtonStyles/ButtonStyles/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isDisabled = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack(spacing: 30) {
+                buttonGroupView
+                toggleView
+            }
+            .navigationTitle("Button Styles")
+            .toolbarTitleDisplayMode(.inline)
+            .padding()
         }
-        .padding()
+    }
+
+    private var buttonGroupView: some View {
+        Group {
+            Button("Automatic") {}
+                .buttonStyle(.automatic)
+            Button("Borderless") {}
+                .buttonStyle(.borderless)
+            Button("Plain") {}
+                .buttonStyle(.plain)
+            Button("Bordered") {}
+                .buttonStyle(.bordered)
+            Button("BorderedProminent") {}
+                .buttonStyle(.borderedProminent)
+        }
+        .disabled(isDisabled)
+    }
+
+    private var toggleView: some View {
+        Toggle(isOn: $isDisabled) {
+            Text("有効/無効に切り替える")
+                .font(.subheadline)
+                .foregroundStyle(.black.opacity(0.8))
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding(.trailing, 5)
+        }
     }
 }
 
