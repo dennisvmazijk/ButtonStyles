fdda94a1b82d4b35f39fc8e546a5d95455f6f50a
Dennis van Mazijk <dennis.v.mazijk@ubiregi.com>
2024-11-18 15:39:47 +0900

    Initial commit

---
+++ ButtonStyles/ButtonStyles/ButtonStylesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonStylesApp.swift
+//  ButtonStyles
+//
+//  Created by Dennis van Mazijk on 2024/11/18.
+//
+
+import SwiftUI
+
+@main
+struct ButtonStylesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
